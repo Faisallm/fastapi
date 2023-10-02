ea60c92eb318595a720977e2c3e44d2f12b814ca
Faisallm <faisallawan1997@gmail.com>
2023-10-02 11:36:46 -0700

    first post api routing added

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
+
+# it's like we are applying magic to a function,
+# home pages
 @app.get('/')
 def root():
 
-    # fastApi will automatically convert output to json
-    return {'message': "Hello Faisal"}
+    # fastApi will automatically output to json
+    return {'message': "Faisal Lawan Muhammad"}
+
+
+@app.get('/posts')
+def get_posts():
+    return {'data-post': "Faisal, This is your posts, All-for-one, One-for-All"}
+
+
+# extract fields from the body (data from the client side)
+# store them as a dictionary on a variable payLoad.
+@app.post('/createposts')
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']},  content: {payLoad['content']}"}  
