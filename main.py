70f40e1c56aee83fcc565f81863dfa88a40acdb6
Faisallm <faisallawan1997@gmail.com>
2023-10-03 23:06:11 -0700

    Addede optional fields, and different field types to my schema

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+my_posts = [{"title": "Machina", "content":"John Wick", "id":1},
+            {"title": "Machine", "content":"Artificial Intelligence", "id":2},]
+# schema
+class Post(BaseModel):
+    title: str
+    content: str
+    # if the user does not provide published
+    # it's default value should be set to published. 
+    published: bool = True
+    # this is an optional field if the user
+    # does not provide it, it's gonna
+    # default to none.
+    rating: Optional[int] = None
+
+
+# the user needs to send the data in a format/scheme
+# that we expect the data to look in a certain way
+# the frontend can't just send random data
+# we can use pydantic to define our schema
 
 
 # it's like we are applying magic to a function,
@@ -15,12 +37,23 @@ def root():
 
 @app.get('/posts')
 def get_posts():
-    return {'data-post': "Faisal, This is your posts, All-for-one, One-for-All"}
+    return {'data-post': my_posts}
 
 
 # extract fields from the body (data from the client side)
 # store them as a dictionary on a variable payLoad.
-@app.post('/createposts')
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title: {payLoad['title']},  content: {payLoad['content']}"}  
+
+# fastapi will automatically validate the data it receives...
+# from the client based off the pydantic schema
+@app.post('/posts') 
+def createposts(post: Post):
+    # converting a pydantic model to dictionary
+    print(post)
+    print(post.dict())
+    print("#############################################################3")
+    print('rating:', post.rating)
+    print('published=', post.published)
+    return {"data": post}
+
+# data required to create post
+# title str, content str, category, published Bool
